c423eee4a07df5ea62db61c1f6b5cdda8606e931
Zain Asif <zainclasher2002@gmail.com>
2022-12-30 19:29:38 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <div>
-
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import logo from "../images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={logo} />
+        </nav>
+    )
+}
