ade008b3950079d70c4703b7daf80a4be27b7a98
Zain Asif <zainclasher2002@gmail.com>
2023-01-01 00:45:41 +0500

    feat: Added a div with cond that renders soldout badge

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,25 @@
 import star from '../images/star.png'
 
 export default function Card(props) {
-    console.log(props)
+    // console.log(props)
     return (
+        // We will be using .props as we pass props
+        // in out Card() parameter or we can use anyother
+        // name but props"also called propersties" is a
+        // better approach
         <div className='card'>
+            {props.openSpots === 0 && <div className='card-badge'>SOLD OUT</div>}
             <img src={`./images/${props.img}`} className='card-image'
                 alt='Katie' />
             <div className='card-stats'>
-                <img src={star} className='card-star' />
+                <img src={star} className='card-star'
+                    alt='star' />
                 <span>{props.rating}</span>
                 <span className='gray'> ({props.reviewCount}) • </span>
-                <span className='gray'>{props.country}</span>
+                <span className='gray'>{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className='bold'>From ${props.price} </span>/ person</p>
+            <p className='card-title'>{props.title}</p>
+            <p className='card-price'><span className='bold'>From ${props.price} </span>/ person</p>
         </div>
     )
 }
