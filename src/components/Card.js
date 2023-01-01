ab6dd89de4cb520b6e9ffc8fb0d82533c9dfd651
Zain Asif <zainclasher2002@gmail.com>
2023-01-02 01:04:14 +0500

    feat: if and else added for BadgeText to display Soldout or online

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,24 +2,31 @@ import star from '../images/star.png'
 
 export default function Card(props) {
     // console.log(props)
+    let bagdeText
+    if (props.item.openSpots === 0) {
+        bagdeText = "SOLD OUT"
+    }
+    else if (props.item.location === "Online") {
+        bagdeText = "ONLINE"
+    }
     return (
         // We will be using .props as we pass props
         // in out Card() parameter or we can use anyother
         // name but props"also called propersties" is a
         // better approach
         <div className='card'>
-            {props.openSpots === 0 && <div className='card-badge'>SOLD OUT</div>}
-            <img src={`./images/${props.img}`} className='card-image'
+            {bagdeText && <div className='card-badge'>{bagdeText}</div>}
+            <img src={`./images/${props.item.coverImg}`} className='card-image'
                 alt='Katie' />
             <div className='card-stats'>
                 <img src={star} className='card-star'
                     alt='star' />
-                <span>{props.rating}</span>
-                <span className='gray'> ({props.reviewCount}) • </span>
-                <span className='gray'>{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className='gray'> ({props.item.stats.reviewCount}) • </span>
+                <span className='gray'>{props.item.location}</span>
             </div>
-            <p className='card-title'>{props.title}</p>
-            <p className='card-price'><span className='bold'>From ${props.price} </span>/ person</p>
+            <p className='card-title'>{props.item.title}</p>
+            <p className='card-price'><span className='bold'>From ${props.item.price} </span>/ person</p>
         </div>
     )
 }
